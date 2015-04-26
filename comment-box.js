b873b9e0c2db729cd199814c558340f61ced539c
Chris Kay <chriskay93@yahoo.co.uk>
2015-04-26 21:13:25 +0100

    updated git

---
+++ comment-box.js
@@ -0,0 +1,32 @@
+(function() {
+    var app = angular.module('blog-commentBox', []);
+
+    app.controller('CommentController', function() {
+        this.showComments = false;
+
+        this.toggleComments = function() {
+            this.showComments = !this.showComments;
+        }
+
+        this.commentsVisible = function(checkComments) {
+            return this.showComments === checkComments;
+        }
+    });
+
+    app.controller('CommentMessageController', function() {
+        this.comment = {};
+
+        this.addComment = function(product) {
+            this.comment.createdOn = (Date.now());
+            product.comments.push(this.comment);
+            this.comment = {};
+        }
+    });
+
+    app.directive('commentBox', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'comment-box.html'
+        }
+    });
+})();
