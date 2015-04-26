cf742c39854ba98183e22e3cc4a5478f2ccc818a
Chris Kay <chriskay93@yahoo.co.uk>
2015-04-26 17:30:34 +0100

    Refactored comment code out of app.js

--- app.js
+++ app.js
@@ -1,37 +1,8 @@
 (function() {
-    var app = angular.module('blog', ['ngSanitize']);
+    var app = angular.module('blog', ['ngSanitize', 'blog-commentBox']);
 
     app.controller('BlogController', function() {
         this.products = posts;
-    });
-
-    app.controller('CommentController', function() {
-        this.showComments = false;
-
-        this.toggleComments = function() {
-            this.showComments = !this.showComments;
-        }
-
-        this.commentsVisible = function(checkComments) {
-            return this.showComments === checkComments;
-        }
-    });
-
-    app.controller('CommentMessageController', function() {
-       this.comment = {};
-
-        this.addComment = function(product) {
-            this.comment.createdOn = (Date.now());
-            product.comments.push(this.comment);
-            this.comment = {};
-        }
-    });
-
-    app.directive('commentBox', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'comment-box.html'
-        }
     });
 
     var posts = [
